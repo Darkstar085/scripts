10c4fdd4f7e56b6aa9df677884ec6c7c25940abc
Sipun Ku Mahanta <sipunkumar85@gmail.com>
2018-04-26 14:23:41 +0530

    Update oreo-nichcream.sh

--- oreo-nichcream.sh
+++ oreo-nichcream.sh
@@ -7,7 +7,6 @@ git clone https://github.com/nichcream/wingtech_kernel_msm8937.git -b 8.1 kernel
 rm -rf vendor/xiaomi
 git clone https://github.com/nichcream/proprietary_vendor_xiaomi.git -b 8.1 vendor/xiaomi
 
-
 #HAL's
 rm -rf hardware/qcom/audio-caf
 git clone https://github.com/TeamReloaded/hardware_qcom_audio.git -b oreo-mr1-8937 hardware/qcom/audio-caf/msm8996 
@@ -18,3 +17,9 @@ git clone https://github.com/TeamReloaded/hardware_qcom_display -b oreo-mr1-8937
 rm -rf hardware/qcom/media-caf 
 git clone https://github.com/TeamReloaded/hardware_qcom_media.git -b oreo-mr1-8937 hardware/qcom/media-caf/msm8996
 git clone https://github.com/TeamReloaded/hardware_qcom_media.git -b oreo-mr1-8937 hardware/qcom/media-caf/msm8937
+
+#Bionic
+cd bionic
+git fetch https://github.com/TeamReloaded/bionic
+git cherry-pick 1e55ab48ba3f3db809163e0476051aad718d70f2^..678dd8ad22225dfef8d76a2e9cac92dc3a843e88
+cd ..
