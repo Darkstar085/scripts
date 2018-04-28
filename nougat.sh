0dd277f18715f9e5d0f935d0a2d3ee2d1be15b81
Sipun Ku Mahanta <sipunkumar85@gmail.com>
2018-04-28 14:58:10 +0530

    Update nougat.sh

--- nougat.sh
+++ nougat.sh
@@ -6,7 +6,7 @@ rm -rf kernel/xiaomi/msm8937
 git clone https://github.com/LineageOS/android_kernel_xiaomi_msm8937.git -b cm-14.1 kernel/xiaomi/msm8937
 rm -rf vendor/xiaomi
 git clone https://github.com/TheMuppets/proprietary_vendor_xiaomi.git -b cm-14.1 vendor/xiaomi
-
+#
 #HAL's
 rm -rf hardware/qcom/audio-caf
 git clone https://github.com/LineageOS/android_hardware_qcom_audio.git -b cm-14.1-caf-8937 hardware/qcom/audio-caf/msm8937
