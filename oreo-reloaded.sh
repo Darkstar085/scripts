26c554f5e823c193681827d2d70729f689f05a28
Sipun Ku Mahanta <sipunkumar85@gmail.com>
2018-04-28 22:13:04 +0530

    land : Fix Up

--- oreo-reloaded.sh
+++ oreo-reloaded.sh
@@ -6,7 +6,6 @@ rm -rf kernel/xiaomi/msm8937
 git clone https://github.com/TeamReloaded/android_kernel_xiaomi_msm8937.git -b lineage-15.1 kernel/xiaomi/msm8937
 rm -rf vendor/xiaomi
 git clone https://github.com/TeamReloaded/proprietary_vendor_xiaomi.git -b lineage-15.1-land vendor/xiaomi
-#
 #HAL's
 rm -rf hardware/qcom/audio-caf
 git clone https://github.com/TeamReloaded/hardware_qcom_audio.git -b oreo-mr1-8937 hardware/qcom/audio-caf/msm8996 
@@ -17,7 +16,6 @@ git clone https://github.com/TeamReloaded/hardware_qcom_display -b oreo-mr1-8937
 rm -rf hardware/qcom/media-caf 
 git clone https://github.com/TeamReloaded/hardware_qcom_media.git -b oreo-mr1-8937 hardware/qcom/media-caf/msm8996
 git clone https://github.com/TeamReloaded/hardware_qcom_media.git -b oreo-mr1-8937 hardware/qcom/media-caf/msm8937
-#
 #Bionic
 cd bionic
 git fetch https://github.com/TeamReloaded/bionic
