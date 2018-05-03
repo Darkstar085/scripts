b548678ce3e7dd2f756456be2795c8975dfa6f75
Sipun Ku Mahanta <sipunkumar85@gmail.com>
2018-05-03 17:39:27 +0530

    darkstar : testing color-ify

--- darkstar.sh
+++ darkstar.sh
@@ -1,6 +1,30 @@
 #!/bin/bash
+# Copyright (C) 2015 Paranoid Android Project
+# Copyright (C) 2018 Darkstar<sipunkumar85@gmail.com)
 
-echo "Syncing device repos"
+# PA Colors
+# red = errors, cyan = warnings, green = confirmations, blue = informational
+# plain for generic text, bold for titles, reset flag at each end of line
+# plain blue should not be used for readability reasons - use plain cyan instead
+CLR_RST=$(tput sgr0)                        ## reset flag
+CLR_RED=$CLR_RST$(tput setaf 1)             #  red, plain
+CLR_GRN=$CLR_RST$(tput setaf 2)             #  green, plain
+CLR_BLU=$CLR_RST$(tput setaf 4)             #  blue, plain
+CLR_CYA=$CLR_RST$(tput setaf 6)             #  cyan, plain
+CLR_BLD=$(tput bold)                        ## bold flag
+CLR_BLD_RED=$CLR_RST$CLR_BLD$(tput setaf 1) #  red, bold
+CLR_BLD_GRN=$CLR_RST$CLR_BLD$(tput setaf 2) #  green, bold
+CLR_BLD_BLU=$CLR_RST$CLR_BLD$(tput setaf 4) #  blue, bold
+CLR_BLD_CYA=$CLR_RST$CLR_BLD$(tput setaf 6) #  cyan, bold
+echo -e ""
+echo -e "${CLR_BLD_BLU}██████╗  █████╗ ██████╗ ██╗  ██╗███████╗████████╗ █████╗ ██████╗ ${CLR_RST}"
+echo -e "${CLR_BLD_BLU}██╔══██╗██╔══██╗██╔══██╗██║ ██╔╝██╔════╝╚══██╔══╝██╔══██╗██╔══██╗${CLR_RST}"
+echo -e "${CLR_BLD_BLU}██║  ██║███████║██████╔╝█████╔╝ ███████╗   ██║   ███████║██████╔╝${CLR_RST}"
+echo -e "${CLR_BLD_BLU}██║  ██║██╔══██║██╔══██╗██╔═██╗ ╚════██║   ██║   ██╔══██║██╔══██╗${CLR_RST}"
+echo -e "${CLR_BLD_BLU}██████╔╝██║  ██║██║  ██║██║  ██╗███████║   ██║   ██║  ██║██║  ██║${CLR_RST}"
+echo -e "${CLR_BLD_BLU}╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝╚══════╝   ╚═╝   ╚═╝  ╚═╝╚═╝  ╚═╝${CLR_RST}"
+echo -e ""
+echo -e "${CLR_BLD_BLU}Syncing device repos${CLR_RST}"
 git clone https://github.com/Sweeto143/android_device_xiaomi_land.git -b oreo device/xiaomi/land
 git clone https://github.com/Sweeto143/android_kernel_xiaomi_msm8937.git -b oreo kernel/xiaomi/msm8937
 git clone https://github.com/Sweeto143/proprietary_vendor_xiaomi.git -b oreo vendor/xiaomi
