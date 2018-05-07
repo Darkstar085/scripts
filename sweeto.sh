aa2551160dc10b853f7f24aa4f5f54fff10560ef
Sipun Ku Mahanta <sipunkumar85@gmail.com>
2018-05-07 15:48:24 +0530

    color-ify

--- sweeto.sh
+++ sweeto.sh
@@ -1,7 +1,31 @@
 #!/bin/bash
+# Copyright (C) 2015 Paranoid Android Project
+
+# PA Colors
+# red = errors, cyan = warnings, green = confirmations, blue = informational
+# plain for generic text, bold for titles, reset flag at each end of line
+# plain blue should not be used for readability reasons - use plain cyan instead
+CLR_RST=$(tput sgr0)                        ## reset flag
+CLR_RED=$CLR_RST$(tput setaf 1)             #  red, plain
+CLR_GRN=$CLR_RST$(tput setaf 2)             #  green, plain
+CLR_YLW=$CLR_RST$(tput setaf 3)             #  yellow, plain
+CLR_BLU=$CLR_RST$(tput setaf 4)             #  blue, plain
+CLR_PPL=$CLR_RST$(tput setaf 5)             #  purple,plain
+CLR_CYA=$CLR_RST$(tput setaf 6)             #  cyan, plain
+CLR_BLD=$(tput bold)                        ## bold flag
+CLR_BLD_RED=$CLR_RST$CLR_BLD$(tput setaf 1) #  red, bold
+CLR_BLD_GRN=$CLR_RST$CLR_BLD$(tput setaf 2) #  green, bold
+CLR_BLD_YLW=$CLR_RST$CLR_BLD$(tput setaf 3) #  yellow, bold
+CLR_BLD_BLU=$CLR_RST$CLR_BLD$(tput setaf 4) #  blue, bold
+CLR_BLD_PPL=$CLR_RST$CLR_BLD$(tput setaf 5) #  purple, bold
+CLR_BLD_CYA=$CLR_RST$CLR_BLD$(tput setaf 6) #  cyan, bold
+echo -e "${CLR_BLD_RED}Syncing device repos...${CLR_RST}"
 git clone https://github.com/Sweeto143/device_xiaomi_land.git -b oreo device/xiaomi/land
 git clone https://github.com/Sweeto143/kernel_xiaomi_msm8937.git -b oreo kernel/xiaomi/msm8937
 git clone https://github.com/Sweeto143/vendor_xiaomi.git -b oreo vendor/xiaomi
+echo -e "${CLR_BLD_RED}Sync Complete...${CLR_RST}"
+echo -e ""
+echo -e "${CLR_BLD_RED}Syncing Hals...${CLR_RST}"
 rm -rf hardware/qcom/audio-caf
 rm -rf hardware/qcom/media-caf
 rm -rf hardware/qcom/display-caf
@@ -11,4 +35,8 @@ git clone https://github.com/TeamReloaded/hardware_qcom_display -b oreo-mr1-8937
 git clone https://github.com/TeamReloaded/hardware_qcom_display -b oreo-mr1-8937 hardware/qcom/display-caf/msm8937
 git clone https://github.com/TeamReloaded/hardware_qcom_media.git -b oreo-mr1-8937 hardware/qcom/media-caf/msm8996
 git clone https://github.com/TeamReloaded/hardware_qcom_media.git -b oreo-mr1-8937 hardware/qcom/media-caf/msm8937
-
+echo -e "${CLR_BLD_RED}Sync Complete...${CLR_RST}"
+echo -e ""
+echo -e "${CLR_BLD_RED}Now You are good to Go${CLR_RST}"
+echo -e ""
+echo -e ""
