c6bcb2a6281d7def4e7b2d7ec30906a60243a91c
Sipun Ku Mahanta <sipunkumar85@gmail.com>
2018-05-07 15:46:23 +0530

    Added Repos

--- sweeto.sh
+++ sweeto.sh
@@ -1,1 +1,14 @@
-#dummy
+#!/bin/bash
+git clone https://github.com/Sweeto143/device_xiaomi_land.git -b oreo device/xiaomi/land
+git clone https://github.com/Sweeto143/kernel_xiaomi_msm8937.git -b oreo kernel/xiaomi/msm8937
+git clone https://github.com/Sweeto143/vendor_xiaomi.git -b oreo vendor/xiaomi
+rm -rf hardware/qcom/audio-caf
+rm -rf hardware/qcom/media-caf
+rm -rf hardware/qcom/display-caf
+git clone https://github.com/TeamReloaded/hardware_qcom_audio.git -b oreo-mr1-8937 hardware/qcom/audio-caf/msm8996
+git clone https://github.com/TeamReloaded/hardware_qcom_audio.git -b oreo-mr1-8937 hardware/qcom/audio-caf/msm8937
+git clone https://github.com/TeamReloaded/hardware_qcom_display -b oreo-mr1-8937 hardware/qcom/display-caf/msm8996
+git clone https://github.com/TeamReloaded/hardware_qcom_display -b oreo-mr1-8937 hardware/qcom/display-caf/msm8937
+git clone https://github.com/TeamReloaded/hardware_qcom_media.git -b oreo-mr1-8937 hardware/qcom/media-caf/msm8996
+git clone https://github.com/TeamReloaded/hardware_qcom_media.git -b oreo-mr1-8937 hardware/qcom/media-caf/msm8937
+
