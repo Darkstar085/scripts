e824e82235223cbd523d12558ae0a6ed4571b3d4
Sipun Ku Mahanta <sipunkumar85@gmail.com>
2018-05-09 21:39:12 +0530

    Update oreo-reloaded.sh

--- oreo-reloaded.sh
+++ oreo-reloaded.sh
@@ -39,20 +39,14 @@ echo -e "${CLR_BLD_PPL}Syncing Hals...${CLR_RST}"
 rm -rf hardware/qcom/audio-caf
 rm -rf hardware/qcom/media-caf
 rm -rf hardware/qcom/display-caf
-git clone https://github.com/TeamReloaded/hardware_qcom_audio.git -b oreo-mr1-8937 hardware/qcom/audio-caf/msm8996
-git clone https://github.com/TeamReloaded/hardware_qcom_audio.git -b oreo-mr1-8937 hardware/qcom/audio-caf/msm8937
-git clone https://github.com/TeamReloaded/hardware_qcom_display -b oreo-mr1-8937 hardware/qcom/display-caf/msm8996
-git clone https://github.com/TeamReloaded/hardware_qcom_display -b oreo-mr1-8937 hardware/qcom/display-caf/msm8937
-git clone https://github.com/TeamReloaded/hardware_qcom_media.git -b oreo-mr1-8937 hardware/qcom/media-caf/msm8996
-git clone https://github.com/TeamReloaded/hardware_qcom_media.git -b oreo-mr1-8937 hardware/qcom/media-caf/msm8937
+git clone https://github.com/TeamReloaded/hardware_qcom_audio.git -b lineage-15.1-caf-8996 hardware/qcom/audio-caf/msm8996
+git clone https://github.com/TeamReloaded/hardware_qcom_audio.git -b lineage-15.1-caf-8996 hardware/qcom/audio-caf/msm8937
+git clone https://github.com/TeamReloaded/hardware_qcom_display.git -b lineage-15.1-caf-8996 hardware/qcom/display-caf/msm8996
+git clone https://github.com/TeamReloaded/hardware_qcom_display.git -b lineage-15.1-caf-8996 hardware/qcom/display-caf/msm8937
+git clone https://github.com/TeamReloaded/hardware_qcom_media.git -b lineage-15.1-caf-8996 hardware/qcom/media-caf/msm8996
+git clone https://github.com/TeamReloaded/hardware_qcom_media.git -b lineage-15.1-caf-8996 hardware/qcom/media-caf/msm8937
 echo -e "${CLR_BLD_PPL}Sync Complete...${CLR_RST}"
 echo -e ""
-echo -e "${CLR_BLD_PPL}Applying Bionic Patch...${CLR_RST}"
-cd bionic
-git fetch https://github.com/TeamReloaded/bionic
-git cherry-pick 1e55ab48ba3f3db809163e0476051aad718d70f2^..678dd8ad22225dfef8d76a2e9cac92dc3a843e88
-cd ..
-echo -e "${CLR_BLD_PPL}Patch Complete...${CLR_RST}"
 echo -e ""
 echo -e "${CLR_BLD_PPL}Now You are good to Go${CLR_RST}"
 echo -e ""
