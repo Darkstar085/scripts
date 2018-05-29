7faca0a9e45d2881e6d700a0f91dc25824d18d98
Sweeto143 <sipunkumar85@gmail.com>
2018-05-29 10:13:00 +0000

    oreo-reloaded.sh : Removed unnecessary HAL's * We use MSM8996 HAL's
    Signed-off-by : Sipun Ku Mahanta<sipunkumar85@gmail.com>

--- oreo-reloaded.sh
+++ oreo-reloaded.sh
@@ -40,11 +40,8 @@ rm -rf hardware/qcom/audio-caf
 rm -rf hardware/qcom/media-caf
 rm -rf hardware/qcom/display-caf
 git clone https://github.com/TeamReloaded/hardware_qcom_audio.git -b lineage-15.1-caf-8996 hardware/qcom/audio-caf/msm8996
-git clone https://github.com/TeamReloaded/hardware_qcom_audio.git -b lineage-15.1-caf-8996 hardware/qcom/audio-caf/msm8937
 git clone https://github.com/TeamReloaded/hardware_qcom_display.git -b lineage-15.1-caf-8996 hardware/qcom/display-caf/msm8996
-git clone https://github.com/TeamReloaded/hardware_qcom_display.git -b lineage-15.1-caf-8996 hardware/qcom/display-caf/msm8937
 git clone https://github.com/TeamReloaded/hardware_qcom_media.git -b lineage-15.1-caf-8996 hardware/qcom/media-caf/msm8996
-git clone https://github.com/TeamReloaded/hardware_qcom_media.git -b lineage-15.1-caf-8996 hardware/qcom/media-caf/msm8937
 echo -e "${CLR_BLD_PPL}Sync Complete...${CLR_RST}"
 echo -e ""
 echo -e ""
