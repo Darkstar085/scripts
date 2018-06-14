1041f832a5142156c3d11a16d8088a44cb41c375
Sipun Ku Mahanta <sipunkumar85@gmail.com>
2018-06-14 16:41:31 +0000

    cardinal.sh: Updated Hals..
    Signed-off-by: Sipun Ku Mahanta <sipunkumar85@gmail.com>

--- cardinal.sh
+++ cardinal.sh
@@ -39,9 +39,9 @@ echo -e "${CLR_BLD_GRN}Cloning Hals...${CLR_RST}"
 rm -rf hardware/qcom/audio-caf
 rm -rf hardware/qcom/media-caf
 rm -rf hardware/qcom/display-caf
-git clone https://github.com/TeamReloaded/hardware_qcom_audio.git -b lineage-15.1-caf-8996 hardware/qcom/audio-caf/msm8996
-git clone https://github.com/TeamReloaded/hardware_qcom_display.git -b lineage-15.1-caf-8996 hardware/qcom/display-caf/msm8996
-git clone https://github.com/TeamReloaded/hardware_qcom_media.git -b lineage-15.1-caf-8996 hardware/qcom/media-caf/msm8996
+git clone https://github.com/Sweeto143/hardware_qcom_audio.git -b oreo hardware/qcom/audio-caf/msm8996
+git clone https://github.com/Sweeto143/hardware_qcom_display.git -b oreo hardware/qcom/display-caf/msm8996
+git clone https://github.com/Sweeto143/hardware_qcom_media.git -b oreo hardware/qcom/media-caf/msm8996
 echo -e "${CLR_BLD_GRN}Cloning Complete...${CLR_RST}"
 echo -e ""
 echo -e "${CLR_BLD_GRN}Now You are good to Go${CLR_RST}"
