b49c84e4d917b4a5a95bf7e29037df7c001af7e1
Sipun Ku Mahanta <sipunkumar85@gmail.com>
2018-06-17 18:49:26 +0000

    git.sh: Added Mine Info
    * This is Only For me..
    
    Signed-off-by: Sipun Ku Mahanta <sipunkumar85@gmail.com>

--- git.sh
+++ git.sh
@@ -1,1 +1,4 @@
-#dummy
+git config --global user.email "sipunkumar85@gmail.com"
+git config --global user.name "Sipun Ku Mahanta"
+git config --global alias.cp 'cherry-pick -s'
+git config --global alias.c 'commit -s'
