f8558b508421d73302e38f70c6e4f3d85a5d6183
B50Corei5 <36875492+B50Corei5@users.noreply.github.com>
2018-06-18 20:22:53 +1200

    Update git.sh

--- git.sh
+++ git.sh
@@ -1,4 +1,4 @@
-git config --global user.email "sipunkumar85@gmail.com"
-git config --global user.name "Sipun Ku Mahanta"
+git config --global user.email "B50Corei5@gmail.com"
+git config --global user.name "B50Corei5"
 git config --global alias.cp 'cherry-pick -s'
 git config --global alias.c 'commit -s'
