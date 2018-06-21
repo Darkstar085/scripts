eb1a1afcff306c08a3912896c27548499a306043
Sipun Ku Mahanta <sipunkumar85@gmail.com>
2018-06-21 15:45:34 +0000

    hals.sh: Added Hals

--- mine/hals.sh
+++ mine/hals.sh
@@ -1,1 +1,6 @@
-#dummy
+rm -rf hardware/qcom/audio-caf
+rm -rf hardware/qcom/media-caf
+rm -rf hardware/qcom/display-caf
+git clone https://github.com/Sweeto143/hardware_qcom_audio.git -b oreo hardware/qcom/audio-caf/msm8996
+git clone https://github.com/Sweeto143/hardware_qcom_display.git -b oreo hardware/qcom/display-caf/msm8996
+git clone https://github.com/Sweeto143/hardware_qcom_media.git -b oreo hardware/qcom/media-caf/msm8996
