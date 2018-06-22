30d47c2a1bd39c9ada36146f49cf9ccfc5228fce
Sipun Ku Mahanta <sipunkumar85@gmail.com>
2018-06-22 08:10:07 +0000

    cardinal.sh: Added Cardinal AOSP ASCII

--- mine/cardinal.sh
+++ mine/cardinal.sh
@@ -37,6 +37,19 @@ repo init -u git://github.com/Cardinal-AOSP/manifest.git -b oreo-mr1 -m crd.xml
 repo sync -c -f -j8 --force-sync --no-clone-bundle --no-tags
 echo -e "${CLR_BLD_GRN}Cloning Complete...${CLR_RST}"
 echo -e ""
+echo -e "${CLR_BLD_GRN} ██████╗ █████╗ ██████╗ ██████╗ ██╗███╗   ██╗ █████╗ ██╗     ${CLR_RST}"
+echo -e "${CLR_BLD_GRN}██╔════╝██╔══██╗██╔══██╗██╔══██╗██║████╗  ██║██╔══██╗██║     ${CLR_RST}"
+echo -e "${CLR_BLD_GRN}██║     ███████║██████╔╝██║  ██║██║██╔██╗ ██║███████║██║     ${CLR_RST}"
+echo -e "${CLR_BLD_GRN}██║     ██╔══██║██╔══██╗██║  ██║██║██║╚██╗██║██╔══██║██║     ${CLR_RST}"
+echo -e "${CLR_BLD_GRN}╚██████╗██║  ██║██║  ██║██████╔╝██║██║ ╚████║██║  ██║███████╗${CLR_RST}"
+echo -e "${CLR_BLD_GRN} ╚═════╝╚═╝  ╚═╝╚═╝  ╚═╝╚═════╝ ╚═╝╚═╝  ╚═══╝╚═╝  ╚═╝╚══════╝${CLR_RST}"
+echo -e "${CLR_BLD_RED}             █████╗  ██████╗ ███████╗██████╗                 ${CLR_RST}"
+echo -e "${CLR_BLD_RED}            ██╔══██╗██╔═══██╗██╔════╝██╔══██╗                ${CLR_RST}"
+echo -e "${CLR_BLD_RED}            ███████║██║   ██║███████╗██████╔╝                ${CLR_RST}"
+echo -e "${CLR_BLD_BLU}            ██╔══██║██║   ██║╚════██║██╔═══╝                 ${CLR_RST}"
+echo -e "${CLR_BLD_BLU}            ██║  ██║╚██████╔╝███████║██║                     ${CLR_RST}"
+echo -e "${CLR_BLD_BLU}            ╚═╝  ╚═╝ ╚═════╝ ╚══════╝╚═╝                     ${CLR_RST}"
+echo -e ""
 echo -e "${CLR_BLD_GRN}Cloning device repos...${CLR_RST}"
 git clone https://github.com/BirdSanctuary/android_device_xiaomi_land.git -b oreo-mr1 device/xiaomi/land
 git clone https://github.com/BirdSanctuary/android_kernel_xiaomi_land.git -b oreo-mr1 kernel/xiaomi/msm8937
