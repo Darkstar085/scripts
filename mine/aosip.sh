610f8f81342280cdae920c9d4043454fa5b1922d
Sipun Ku Mahanta <sipunkumar85@gmail.com>
2018-06-23 02:10:19 +0000

    Added source to AOSiP.sh

--- mine/aosip.sh
+++ mine/aosip.sh
@@ -1,1 +1,14 @@
-#dummy
+cd ~
+mkdir aosip
+cd aosip
+
+
+repo init -u git://github.com/AOSiP/platform_manifest.git -b oreo-mr1
+repo sync -f --force-sync --no-tags --no-clone-bundle
+
+
+git clone https://github.com/Sweeto143/android_device_xiaomi_land-aosip.git -b oreo-mr1 device/xiaomi/land
+git clone https://github.com/Sweeto143/android_kernel_xiaomi_land.git -b oreo-mr1 kernel/xiaomi/msm8937
+git clone https://github.com/Sweeto143/proprietary_vendor_xiaomi_land.git -b oreo vendor/xiaomi
+
+
