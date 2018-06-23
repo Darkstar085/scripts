34d91b6ac2a0d73078f6d58032536b2a883c5166
Sipun Ku Mahanta <sipunkumar85@gmail.com>
2018-06-23 02:23:32 +0000

    aosip.sh: Added ASCII

--- mine/aosip.sh
+++ mine/aosip.sh
@@ -22,7 +22,12 @@ CLR_BLD_BLU=$CLR_RST$CLR_BLD$(tput setaf 4) #  blue, bold
 CLR_BLD_PPL=$CLR_RST$CLR_BLD$(tput setaf 5) #  purple, bold
 CLR_BLD_CYA=$CLR_RST$CLR_BLD$(tput setaf 6) #  cyan, bold
 echo -e ""
-
+echo -e "${CLR_BLD_RED}██████╗  █████╗ ██████╗ ██╗  ██╗███████╗████████╗ █████╗ ██████╗ ${CLR_RST}"
+echo -e "${CLR_BLD_RED}██╔══██╗██╔══██╗██╔══██╗██║ ██╔╝██╔════╝╚══██╔══╝██╔══██╗██╔══██╗${CLR_RST}"
+echo -e "${CLR_BLD_RED}██║  ██║███████║██████╔╝█████╔╝ ███████╗   ██║   ███████║██████╔╝${CLR_RST}"
+echo -e "${CLR_BLD_RED}██║  ██║██╔══██║██╔══██╗██╔═██╗ ╚════██║   ██║   ██╔══██║██╔══██╗${CLR_RST}"
+echo -e "${CLR_BLD_RED}██████╔╝██║  ██║██║  ██║██║  ██╗███████║   ██║   ██║  ██║██║  ██║${CLR_RST}"
+echo -e "${CLR_BLD_RED}╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝╚══════╝   ╚═╝   ╚═╝  ╚═╝╚═╝  ╚═╝${CLR_RST}"
 echo -e ""
 echo -e "${CLR_BLD_PPL}Cloning AOSiP Source Files...${CLR_RST}"
 cd ~
@@ -31,6 +36,15 @@ cd aosip
 repo init -u git://github.com/AOSiP/platform_manifest.git -b oreo-mr1
 repo sync -f --force-sync --no-tags --no-clone-bundle
 echo -e "${CLR_BLD_PPL}Cloning Complete...${CLR_RST}"
+echo -e ""
+echo -e "${CLR_BLD_RED}   ▄████████  ▄██████▄     ▄████████  ▄█     ▄███████▄ ${CLR_RST}"
+echo -e "${CLR_BLD_RED}  ███    ███ ███    ███   ███    ███ ███    ███    ███ ${CLR_RST}"
+echo -e "${CLR_BLD_RED}  ███    ███ ███    ███   ███    █▀  ███▌   ███    ███ ${CLR_RST}"
+echo -e "${CLR_BLD_RED}  ███    ███ ███    ███   ███        ███▌   ███    ███ ${CLR_RST}"
+echo -e "${CLR_BLD_RED}▀███████████ ███    ███ ▀███████████ ███▌ ▀█████████▀  ${CLR_RST}"
+echo -e "${CLR_BLD_RED}  ███    ███ ███    ███          ███ ███    ███        ${CLR_RST}"
+echo -e "${CLR_BLD_RED}  ███    ███ ███    ███    ▄█    ███ ███    ███        ${CLR_RST}"
+echo -e "${CLR_BLD_RED}  ███    █▀   ▀██████▀   ▄████████▀  █▀    ▄████▀      ${CLR_RST}"
 echo -e ""
 echo -e "${CLR_BLD_PPL}Cloning device repos...${CLR_RST}"
 git clone https://github.com/Sweeto143/android_device_xiaomi_land-aosip.git -b oreo-mr1 device/xiaomi/land
