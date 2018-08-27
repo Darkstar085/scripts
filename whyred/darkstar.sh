0d7300a4505e16bb7f865ee0b8f5b5843de43875
Sipun Ku Mahanta <sipunkumar85@gmail.com>
2018-08-27 23:31:09 +0530

    whyred: Added Dragon TC & Miui camera

--- whyred/darkstar.sh
+++ whyred/darkstar.sh
@@ -1,3 +1,5 @@
 git clone https://github.com/Sweeto143/device_xiaomi_whyred.git -b oreo device/xiaomi/whyred
 git clone https://github.com/Sweeto143/kernel_xiaomi_whyred.git -b oreo kernel/xiaomi/sdm660
 git clone https://github.com/Sweeto143/vendor_xiaomi_whyred.git -b oreo vendor/xiaomi
+git clone https://github.com/Sweeto143/prebuilts_clang_host_linux-x86_7.0-DragonTC.git -b master prebuilts/clang/host/linux-x86/7.0-DragonTC
+git clone https://github.com/Sweeto143/vendor_xiaomi_MiuiCamera.git -b oreo vendor/xiaomi/MiuiCamera
