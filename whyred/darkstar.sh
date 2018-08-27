6e051aaa22d73b5d3ac4d7a8e2654754e67d9a28
Sipun Ku Mahanta <sipunkumar85@gmail.com>
2018-08-27 23:21:38 +0530

    whyred: Added Repos

--- whyred/darkstar.sh
+++ whyred/darkstar.sh
@@ -1,1 +1,3 @@
-#dummy
+git clone https://github.com/Sweeto143/device_xiaomi_whyred.git -b oreo device/xiaomi/whyred
+git clone https://github.com/Sweeto143/kernel_xiaomi_whyred.git -b oreo kernel/xiaomi/sdm660
+git clone https://github.com/Sweeto143/vendor_xiaomi_whyred.git -b oreo vendor/xiaomi
