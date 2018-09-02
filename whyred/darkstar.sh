93c502f40a6469276c2700e8de7f924a4a82eeb9
Sipun Ku Mahanta <sipunkumar85@gmail.com>
2018-09-02 16:03:27 +0530

    Added Clang 7.0.2

--- whyred/darkstar.sh
+++ whyred/darkstar.sh
@@ -2,4 +2,5 @@ git clone https://github.com/Sweeto143/device_xiaomi_whyred.git -b oreo device/x
 git clone https://github.com/Sweeto143/kernel_xiaomi_whyred.git -b oreo kernel/xiaomi/sdm660
 git clone https://github.com/Sweeto143/vendor_xiaomi_whyred.git -b oreo vendor/xiaomi
 git clone https://github.com/Sweeto143/prebuilts_clang_host_linux-x86_7.0-DragonTC.git -b master prebuilts/clang/host/linux-x86/7.0-DragonTC
+git clone https://github.com/Sweeto143/clang-prebuilt.git -b 7.0.2 prebuilts/clang/host/linux-x86/clang-7.0.2
 git clone https://github.com/Sweeto143/vendor_xiaomi_MiuiCamera.git -b oreo vendor/xiaomi/MiuiCamera
