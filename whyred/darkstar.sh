e6cae7ef1090417ae09174f42a199757b8f05548
Sipun Ku Mahanta <sipunkumar85@gmail.com>
2018-09-05 16:12:09 +0530

    Some Cleanup

--- whyred/darkstar.sh
+++ whyred/darkstar.sh
@@ -1,6 +1,49 @@
+#!/bin/bash
+# Copyright (C) 2015 Paranoid Android Project
+# Copyright (C) 2018 Sipun Ku Mahanta<sipunkumar85@gmail.com>
+# Copyright (C) 2018 Sweeto143@github / Darkstar085@XDA
+
+# PA Colors
+# red = errors, cyan = warnings, green = confirmations, blue = informational
+# plain for generic text, bold for titles, reset flag at each end of line
+# plain blue should not be used for readability reasons - use plain cyan instead
+CLR_RST=$(tput sgr0)                        ## reset flag
+CLR_RED=$CLR_RST$(tput setaf 1)             #  red, plain
+CLR_GRN=$CLR_RST$(tput setaf 2)             #  green, plain
+CLR_YLW=$CLR_RST$(tput setaf 3)             #  yellow, plain
+CLR_BLU=$CLR_RST$(tput setaf 4)             #  blue, plain
+CLR_PPL=$CLR_RST$(tput setaf 5)             #  purple,plain
+CLR_CYA=$CLR_RST$(tput setaf 6)             #  cyan, plain
+CLR_BLD=$(tput bold)                        ## bold flag
+CLR_BLD_RED=$CLR_RST$CLR_BLD$(tput setaf 1) #  red, bold
+CLR_BLD_GRN=$CLR_RST$CLR_BLD$(tput setaf 2) #  green, bold
+CLR_BLD_YLW=$CLR_RST$CLR_BLD$(tput setaf 3) #  yellow, bold
+CLR_BLD_BLU=$CLR_RST$CLR_BLD$(tput setaf 4) #  blue, bold
+CLR_BLD_PPL=$CLR_RST$CLR_BLD$(tput setaf 5) #  purple, bold
+CLR_BLD_CYA=$CLR_RST$CLR_BLD$(tput setaf 6) #  cyan, bold
+echo -e ""
+echo -e "${CLR_BLD_RED}██████╗  █████╗ ██████╗ ██╗  ██╗███████╗████████╗ █████╗ ██████╗ ${CLR_RST}"
+echo -e "${CLR_BLD_RED}██╔══██╗██╔══██╗██╔══██╗██║ ██╔╝██╔════╝╚══██╔══╝██╔══██╗██╔══██╗${CLR_RST}"
+echo -e "${CLR_BLD_RED}██║  ██║███████║██████╔╝█████╔╝ ███████╗   ██║   ███████║██████╔╝${CLR_RST}"
+echo -e "${CLR_BLD_RED}██║  ██║██╔══██║██╔══██╗██╔═██╗ ╚════██║   ██║   ██╔══██║██╔══██╗${CLR_RST}"
+echo -e "${CLR_BLD_RED}██████╔╝██║  ██║██║  ██║██║  ██╗███████║   ██║   ██║  ██║██║  ██║${CLR_RST}"
+echo -e "${CLR_BLD_RED}╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝╚══════╝   ╚═╝   ╚═╝  ╚═╝╚═╝  ╚═╝${CLR_RST}"
+echo -e ""
+echo -e "${CLR_BLD_RED}Cloning device repos...${CLR_RST}"
 git clone https://github.com/Sweeto143/device_xiaomi_whyred.git -b oreo device/xiaomi/whyred
-git clone https://github.com/Sweeto143/kernel_xiaomi_whyred.git -b oreo kernel/xiaomi/sdm660
+git clone https://github.com/akhilnarang/whyred.git -b oreo-mr1 kernel/xiaomi/sdm660
 git clone https://github.com/Sweeto143/vendor_xiaomi_whyred.git -b oreo vendor/xiaomi
+echo -e "${CLR_BLD_RED}Cloning Complete...${CLR_RST}"
+echo -e ""
+echo -e "${CLR_BLD_RED}Cloning Toolchains...${CLR_RST}"
 git clone https://github.com/Sweeto143/prebuilts_clang_host_linux-x86_7.0-DragonTC.git -b master prebuilts/clang/host/linux-x86/7.0-DragonTC
 git clone https://github.com/Sweeto143/clang-prebuilt.git -b 7.0.2 prebuilts/clang/host/linux-x86/clang-7.0.2
+echo -e "${CLR_BLD_RED}Cloning Complete...${CLR_RST}"
+echo -e ""
+echo -e "${CLR_BLD_RED}Cloning MIUI camera...${CLR_RST}"
 git clone https://github.com/Sweeto143/vendor_xiaomi_MiuiCamera.git -b oreo vendor/xiaomi/MiuiCamera
+echo -e "${CLR_BLD_RED}Cloning Complete...${CLR_RST}"
+echo -e ""
+echo -e "${CLR_BLD_RED}Now You are good to Go${CLR_RST}"
+echo -e ""
+echo -e ""
