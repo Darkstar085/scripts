d2151a2d0593285acf4826946524c8114d102408
Sipun Ku Mahanta <sipunkumar85@gmail.com>
2018-09-13 00:04:25 +0530

    whyred: darkstar.sh: Track Kernel from My git

--- whyred/darkstar.sh
+++ whyred/darkstar.sh
@@ -31,12 +31,11 @@ echo -e "${CLR_BLD_RED}╚═════╝ ╚═╝  ╚═╝╚═╝  ╚�
 echo -e ""
 echo -e "${CLR_BLD_RED}Cloning device repos...${CLR_RST}"
 git clone https://github.com/Sweeto143/device_xiaomi_whyred.git -b oreo device/xiaomi/whyred
-git clone https://github.com/akhilnarang/whyred.git -b oreo-mr1 kernel/xiaomi/sdm660
+git clone https://github.com/Sweeto143/kernel_xiaomi_whyred.git -b oreo kernel/xiaomi/whyred
 git clone https://github.com/Sweeto143/vendor_xiaomi_whyred.git -b oreo vendor/xiaomi
 echo -e "${CLR_BLD_RED}Cloning Complete...${CLR_RST}"
 echo -e ""
 echo -e "${CLR_BLD_RED}Cloning Toolchains...${CLR_RST}"
-git clone https://github.com/Sweeto143/prebuilts_clang_host_linux-x86_7.0-DragonTC.git -b master prebuilts/clang/host/linux-x86/7.0-DragonTC
 git clone https://github.com/Sweeto143/clang-prebuilt.git -b 7.0.2 prebuilts/clang/host/linux-x86/clang-7.0.2
 echo -e "${CLR_BLD_RED}Cloning Complete...${CLR_RST}"
 echo -e ""
