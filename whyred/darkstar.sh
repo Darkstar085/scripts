4d9fceea2df7bca9e7b405a62114800e4a8c5215
Sipun Ku Mahanta <sipunkumar85@gmail.com>
2021-07-24 12:43:19 +0530

    superior: Again new trees
    Signed-off-by: Sipun Ku Mahanta <sipunkumar85@gmail.com>

--- whyred/darkstar.sh
+++ whyred/darkstar.sh
@@ -140,15 +140,19 @@ elif [ $1 = "superior" ]
   then
 echo -e "${CLR_BLD_RED}Removing device repos...${CLR_RST}"
 rm -rf device/xiaomi/whyred
+rm -rf device/xiaomi/sdm660-common
 rm -rf kernel/xiaomi/whyred
 rm -rf vendor/xiaomi/whyred
+rm -rf vendor/xiaomi/sdm660-common
 rm -rf vendor/MiuiCamera
 echo -e "${CLR_BLD_RED}Device repos removed ...${CLR_RST}"
 echo -e ""
 echo -e "${CLR_BLD_RED}Cloning device repos...${CLR_RST}"
 git clone https://github.com/SuperiorOS-Devices/device_xiaomi_whyred.git -b eleven device/xiaomi/whyred
+git clone https://github.com/SuperiorOS-Devices/device_xiaomi_whyred-common.git -b eleven device/xiaomi/sdm660-common
 git clone https://github.com/SuperiorOS-Devices/kernel_xiaomi_whyred.git -b eleven kernel/xiaomi/whyred
 git clone https://github.com/SuperiorOS-Devices/vendor_xiaomi_whyred.git -b eleven vendor/xiaomi/whyred
+git clone https://github.com/SuperiorOS-Devices/vendor_xiaomi_whyred-common.git -b eleven vendor/xiaomi/sdm660-common
 git clone https://github.com/Darkstar085/vendor_xiaomi_MiuiCamera.git -b eleven vendor/MiuiCamera
 echo -e "${CLR_BLD_RED}Cloning Complete...${CLR_RST}"
 echo -e ""
