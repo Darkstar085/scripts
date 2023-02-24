9502ff4d9bed05be23ab524a6d67bdbf448ed47b
Sipun Ku Mahanta <sipunkumar85@gmail.com>
2023-02-24 08:10:35 +0100

    whyred: add a build script
    Signed-off-by: Sipun Ku Mahanta <sipunkumar85@gmail.com>
    Change-Id: Id6172dcff5c8d917ba918d68725c88b33aaf55e3

---
+++ whyred/build.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Set the rom name
+rom="$1"
+
+# Build the rom by the command input
+if [ "$rom" = "superior" ]
+then
+  . build/envsetup.sh
+  export BUILD_WITH_GAPPS=true
+  export TZ=Asia/Kolkata
+  export KBUILD_BUILD_USER="Sweeto"
+  export KBUILD_BUILD_HOST="Yui"
+  lunch superior_whyred-userdebug
+  mka bacon -j$(nproc --all) | tee build.log
+elif [ "$rom" = "rice" ]
+then
+  . build/envsetup.sh
+  export TZ=Asia/Kolkata
+  export KBUILD_BUILD_USER="Sweeto"
+  export KBUILD_BUILD_HOST="Yui"
+  lunch lineage_whyred-userdebug
+  mka bacon -j$(nproc --all) | tee build.log
+fi
